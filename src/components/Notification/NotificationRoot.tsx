2a3ac2016978633051153770551650314686f387
Flávio Dutra <flaviomdutra@outlook.com>
2024-01-24 22:49:13 -0300

    Refactor Widget component to use Notification component

---
+++ src/components/Notification/NotificationRoot.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+
+interface NotificationRootProps {
+  children: ReactNode;
+}
+
+export function NotificationRoot({
+  children,
+}: Readonly<NotificationRootProps>) {
+  return (
+    <div className="bg-zinc-200 dark:bg-zinc-900 px-8 py-4 flex items-start gap-6">
+      {children}
+    </div>
+  );
+}
--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,4 +1,5 @@
-import { Check, Rocket, X } from "lucide-react";
+import { Rocket } from "lucide-react";
+import { Notification } from "./Notification";
 
 export function Widget() {
   return (
@@ -15,41 +16,12 @@ export function Widget() {
           Recentes
         </div>
 
-        <div className="divide-y-2 divide-zinc-300 darK:divide-zinc-950">
-          <div className="bg-zinc-200 dark:bg-zinc-900 px-8 py-4 flex items-start gap-6">
-            <Rocket className="w-6 h-6 text-violet-500 mt-3" />
-            <div className="flex-1 flex flex-col gap-2">
-              <p className="text-sm leading-relaxed text-zinc-600 dark:text-zinc-100">
-                Você recebeu um convite para fazer parte da empresa Rocketseat.
-              </p>
-              <div className="text-xxs text-zinc-400 flex items-center gap-1">
-                <span>Convite</span>
-                <span>Há 3 min</span>
-              </div>
-            </div>
-          </div>
-
-          <div className="bg-zinc-200 dark:bg-zinc-900 px-8 py-4 flex items-start gap-6">
-            <Rocket className="w-6 h-6 text-violet-500 mt-3" />
-            <div className="flex-1 flex flex-col gap-2">
-              <p className="text-sm leading-relaxed text-zinc-600 dark:text-zinc-100">
-                Você recebeu um convite para fazer parte da empresa Rocketseat.
-              </p>
-              <div className="text-xxs text-zinc-400 flex items-center gap-1">
-                <span>Convite</span>
-                <span>Há 3 min</span>
-              </div>
-            </div>
-
-            <div className="flex gap-2 self-center">
-              <button className="w-8 h-8 rounded flex items-center justify-center bg-zinc-400 hover:bg-zinc-500 dark:bg-zinc-800 dark:hover:bg-zinc-700">
-                <X className="w-3 h-3 text-zinc-50" />
-              </button>
-              <button className="w-8 h-8 rounded flex items-center justify-center bg-violet-500 hover:bg-violet-600">
-                <Check className="w-3 h-3 text-zinc-50" />
-              </button>
-            </div>
-          </div>
+        <div className="divide-y-2 divide-zinc-300 dark:divide-zinc-950">
+          <Notification.Root>
+            <Notification.Icon icon={Rocket} />
+            <Notification.Content text="Você recebeu um convite para participar de um grupo" />
+            <Notification.Actions />
+          </Notification.Root>
         </div>
       </div>
 
@@ -58,42 +30,7 @@ export function Widget() {
           Antigas
         </div>
 
-        <div className="divide-y-2 divide-zinc-300 darK:divide-zinc-950">
-          <div className="bg-zinc-200 dark:bg-zinc-900 px-8 py-4 flex items-start gap-6">
-            <Rocket className="w-6 h-6 text-violet-500 mt-3" />
-            <div className="flex-1 flex flex-col gap-2">
-              <p className="text-sm leading-relaxed text-zinc-600 dark:text-zinc-100">
-                Você recebeu um convite para fazer parte da empresa Rocketseat.
-              </p>
-              <div className="text-xxs text-zinc-400 flex items-center gap-1">
-                <span>Convite</span>
-                <span>Há 3 min</span>
-              </div>
-            </div>
-          </div>
-
-          <div className="bg-zinc-200 dark:bg-zinc-900 px-8 py-4 flex items-start gap-6">
-            <Rocket className="w-6 h-6 text-violet-500 mt-3" />
-            <div className="flex-1 flex flex-col gap-2">
-              <p className="text-sm leading-relaxed text-zinc-600 dark:text-zinc-100">
-                Você recebeu um convite para fazer parte da empresa Rocketseat.
-              </p>
-              <div className="text-xxs text-zinc-400 flex items-center gap-1">
-                <span>Convite</span>
-                <span>Há 3 min</span>
-              </div>
-            </div>
-
-            <div className="flex gap-2 self-center">
-              <button className="w-8 h-8 rounded flex items-center justify-center bg-zinc-400 hover:bg-zinc-500 dark:bg-zinc-800 dark:hover:bg-zinc-700">
-                <X className="w-3 h-3 text-zinc-50" />
-              </button>
-              <button className="w-8 h-8 rounded flex items-center justify-center bg-violet-500 hover:bg-violet-600">
-                <Check className="w-3 h-3 text-zinc-50" />
-              </button>
-            </div>
-          </div>
-        </div>
+        <div className="divide-y-2 divide-zinc-300 dark:divide-zinc-950"></div>
       </div>
     </div>
   );
